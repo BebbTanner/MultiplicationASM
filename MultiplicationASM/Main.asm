982517653d04eca80ea404fbd6eaba1d94319b5d
Tanner Bebb <17tbebb@gmail.com>
2024-02-26 08:51:34 -0800

    Declared and defined the variables that I will using for this program.

--- MultiplicationASM/Main.asm
+++ MultiplicationASM/Main.asm
@@ -1,9 +1,16 @@
+;Tanner Bebb
+;Assembly Multiplication Practice
+
 .386
 Include Irvine32.inc 
 .stack 4096
 ExitProcess proto dwExitCode:dword
 
 .data
+	X SDWORD 3										;int x = 3
+	A SDWORD 7										;int a = 7
+	B SDWORD 5										;int b = 5
+	Y SDWORD 102									;int y = 102
 .code
 
 main PROC
